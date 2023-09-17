4b5edf0f4a6c33366fd477174b772417001b15aa
MOHAMEDZAYEDSCU <118567156+MOHAMEDZAYEDSCU@users.noreply.github.com>
2023-09-17 09:19:01 +0300

    Modified

--- testing.c
+++ testing.c
@@ -7,31 +7,18 @@
  */
 int main(void)
 {
+_printf("Negative:[%d]\n", -762534);
+printf("Negative:[%d]\n", -762534);
 
-  	_printf("Negative:[%d]\n", -762534);
-  	printf("Negative:[%d]\n", -762534);
+_printf("Character:[%c]\n", 'H');
+printf("Character:[%c]\n", 'H');
+_printf("character [%c]\n", 65);
+_printf("String:[%s]\n", "I am a string !");
+printf("String:[%s]\n", "I am a string !");
+_printf(NULL);
+_printf("\n");
+_printf("%s\n", NULL);
 
-    	_printf("Character:[%c]\n", 'H');
-    	printf("Character:[%c]\n", 'H');
-  	_printf("character [%c]\n", 65);
- 	 _printf("String:[%s]\n", "I am a string !");
-  	 printf("String:[%s]\n", "I am a string !");
-	_printf(NULL);
-	_printf("\n");
-	_printf("%s\n", NULL);
-
-    return (0);
+return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
