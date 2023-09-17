0c9bdb9db75010c17f26f37f334b11bce671067f
Ahmedmo1242 <Ahmedmmahmoud0@gmail.com>
2023-09-17 08:35:05 -0700

    Updated printf , and moved zayed printf to my file to check

--- _printf.c
+++ _printf.c
@@ -1,25 +1,105 @@
 #include "main.h"
 
 /**
- * _printf - clone version of printf
+ * _printf - Custom printf function.
+ * @format: Format specifier string.
+ * @...: Variable number of arguments to format and print.
  *
- * @format: a pointer to the format and text
- *
- * Return: 0 in successful
- *		-1 in failure.
+ * Return: Number of characters printed (excluding null byte).
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	va_list args;
+    va_list args;
+    int count = 0;
+    const char *ptr;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	if (format == NULL)
-		return (-1);
+    for (ptr = format; *ptr != '\0'; ptr++)
+    {
+        if (*ptr == '%')
+        {
+            ptr++;
 
-	i = _vprintf(format, args);
+            if (*ptr == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*ptr == 's')
+            {
+                char *str = va_arg(args, char *);
+                for (int i = 0; str[i] != '\0'; i++)
+                {
+                    write(1, &str[i], 1);
+                    count++;
+                }
+            }
+            else if (*ptr == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else if (*ptr == 'd' || *ptr == 'i')
+            {
+                int num = va_arg(args, int);
+                char num_str[20];
+                int len = sprintf(num_str, "%d", num);
+                write(1, num_str, len);
+                count += len;
+            }
+            else if (*ptr == 'b')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char binary_str[33];
+                for (int i = 31; i >= 0; i--)
+                {
+                    binary_str[31 - i] = ((num >> i) & 1) ? '1' : '0';
+                }
+                binary_str[32] = '\0';
+                write(1, binary_str, 32);
+                count += 32;
+            }
+            else if (*ptr == 'u')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char num_str[20];
+                int len = sprintf(num_str, "%u", num);
+                write(1, num_str, len);
+                count += len;
+            }
+            else if (*ptr == 'o')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char num_str[20];
+                int len = sprintf(num_str, "%o", num);
+                write(1, num_str, len);
+                count += len;
+            }
+            else if (*ptr == 'x' || *ptr == 'X')
+            {
+                unsigned int num = va_arg(args, unsigned int);
+                char num_str[20];
+                int len;
 
-	va_end(args);
-	return (i);
+                if (*ptr == 'x')
+                    len = sprintf(num_str, "%x", num);
+                else
+                    len = sprintf(num_str, "%X", num);
+
+                write(1, num_str, len);
+                count += len;
+            }
+        }
+        else
+        {
+            write(1, ptr, 1);
+            count++;
+        }
+    }
+
+    va_end(args);
+
+    return count;
 }
