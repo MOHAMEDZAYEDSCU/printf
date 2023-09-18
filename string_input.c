6ccbdd216a3069400cfa703a47df1709eae06f7c
Ahmedmo1242 <Ahmedmmahmoud0@gmail.com>
2023-09-18 16:04:09 -0700

    string

--- string_input.c
+++ string_input.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * _putstr - Prints a string to the standard output
+ * _putstr - Prints a string to the standard output.
  *
- * @string: The string to print
+ * @string: The string to print.
  *
- * Return: The length of the string
+ * Return: The length of the string.
  */
-
 int _putstr(char *string)
 {
-	char *current_char = string;
-	int length = 0;
+    char *current_char = string;
+    int length = 0;
 
-	if (!current_char)
-		current_char = "(null)";
+    if (!current_char)
+        current_char = "(null)";
 
-	while (*current_char)
-	{
-		_putchar(*current_char);
-		length++;
-		current_char++;
-	}
+    while (*current_char)
+    {
+        _putchar(*current_char);
+        length++;
+        current_char++;
+    }
 
-	return (length);
+    return (length);
 }
