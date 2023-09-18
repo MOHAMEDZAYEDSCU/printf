6192e7de77747f92b8331031c8431939f5043bfa
MOHAMEDZAYEDSCU <118567156+MOHAMEDZAYEDSCU@users.noreply.github.com>
2023-09-18 11:10:49 +0300

    Fixed %\0

--- _printf.c
+++ _printf.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * printf - Prints formatted data to the standard output
+ * _printf - Prints formatted data to the standard output
+ *
  * @format: The format string containing the types and characters to print
  *
  * Return: The length of the formatted output
@@ -10,6 +11,10 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int length;
+
+	if (format == NULL || (format[0] == '%' && !format[1]) ||
+			(format[0] == '%' && format[1] == ' ' && !format[2]))
+		return (-1);
 
 	va_start(args, format);
 	length = vprintf(format, args);
