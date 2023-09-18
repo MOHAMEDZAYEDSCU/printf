df5ef472a68dd0d5ac38a3e5a85ec67f49445993
Ahmedmo1242 <Ahmedmmahmoud0@gmail.com>
2023-09-17 17:05:51 -0700

    editing

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+   _printf("Let's print a simple sentence."); 
+    _printf("%s", "I am a string !");
+    _printf("%c", 'S'); 
+    _printf("%%"); 
+    return (0);
+}
