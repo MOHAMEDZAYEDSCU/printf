9956e5facaf2036b49e9196ef71729ae8864e777
Ahmedmo1242 <Ahmedmmahmoud0@gmail.com>
2023-09-17 18:26:13 -0700

    int

--- int_input.c
+++ int_input.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print_integer - Prints an integer to the standard output
+ * _putint - Prints an integer to the standard output
  * @num: The integer to print
  *
  * Return: The number of digits in the integer
  */
-
 int _putint(int num)
 {
-	unsigned int abs_num = (unsigned int)num;
+	unsigned int abs_num =  num;
 	int digit_count = 0;
 
 	if (!abs_num)
@@ -21,12 +20,12 @@ int _putint(int num)
 	if (num < 0)
 	{
 		digit_count += _putchar('-');
-		abs_num = (unsigned int)(-num);
+		abs_num = -num;
 	}
 
 	if ((abs_num / 10) > 0)
 	{
-		print_integer((int)(abs_num / 10));
+		_putint((abs_num / 10));
 	}
 	_putchar('0' + (abs_num % 10));
 
