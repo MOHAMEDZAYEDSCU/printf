c0b0372f95b13e71a14eab6c49915776bcbe3cdf
Ahmedmo1242 <Ahmedmmahmoud0@gmail.com>
2023-09-18 22:09:48 -0700

    str

--- string_input.c
+++ string_input.c
@@ -4,19 +4,19 @@
 /**
  * _putstr - Prints a string on the standard output.
  * @string: The string to print.
+ * @count: pointer to the counter
  *
  * Return: The length of the string.
  */
-int _putstr(char *string)
+void _putstr(char *str, int *count)
 {
-    char *s = string;
-    int i = 0;
-
-    if (!s)
-        s = "(null)";
-
-    while (s[i])
-        _putchar(s[i++]);
-
-    return (i);
+    if (str == NULL)
+        _putstr("(null)", count);
+    else
+		while (*str)
+		{
+			_putchar(*str);
+			str++;
+            (*count)++;
+		}
 }
